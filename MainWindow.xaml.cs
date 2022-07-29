6e17a14d76b53f010fc4bf0e5dd61e8d81540bec
Siddhant Madhur <siddhant.madhur@gmail.com>
2022-07-29 23:37:45 +0400

    made it so data is saved in a text file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace RemindersLite
 {
@@ -23,6 +24,53 @@ namespace RemindersLite
         public MainWindow()
         {
             InitializeComponent();
+            init();
+        }
+
+        private void init ()
+        {
+            string[] tasks = File.ReadAllLines("data.txt");
+            foreach (string task in tasks)
+            {
+                listOfReminders.Items.Add(task);
+            }
+        }
+
+        private void addNewTask(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(reminderInput.Text) && (!listOfReminders.Items.Contains(reminderInput.Text))){
+                listOfReminders.Items.Add(reminderInput.Text);
+                reminderInput.Clear();
+                if (listOfReminders.Items.Count > 0)
+                {
+                    using(TextWriter TW = new StreamWriter("data.txt"))
+                    {
+                        foreach(string itemText in listOfReminders.Items)
+                        {
+                            TW.WriteLine(itemText);
+                        }
+                    }
+
+                    
+                }
+            }
+        }
+
+        private void deleteTask(object sender, RoutedEventArgs e)
+        {
+            listOfReminders.Items.RemoveAt(listOfReminders.Items.IndexOf(listOfReminders.SelectedItem));
+            if (listOfReminders.Items.Count >= 0)
+            {
+                using (TextWriter TW = new StreamWriter("data.txt"))
+                {
+                    foreach (string itemText in listOfReminders.Items)
+                    {
+                        TW.WriteLine(itemText);
+                    }
+                }
+
+
+            }
         }
     }
 }
